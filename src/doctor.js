a838576cf2fc636951e042f7065c5c1f42dc6c65
wymerkd <wymerkd@gmail.com>
2019-11-24 19:05:56 -0800

    replace API key in URL with process.env.API_Key

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,7 @@
 export class Doctors {
   async findDoctors()) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=f42c94ad008bb7ce74679e9d77369825`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {

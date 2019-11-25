7d748e672bcc364d53729669bf8fb2eb3423a24b
wymerkd <wymerkd@gmail.com>
2019-11-24 21:28:25 -0800

    add variable for symptom and doctor name and add inputs to form to search by name and symptom.

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,7 @@
 export class Doctors {
-  async findDoctors()) {
+  async findDoctors(doctorName, userSymptom) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${userSymptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import 'bootstrap';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#symptom-button').click(function() {
+  $('#submit').click(function() {
     const userSymptom = $("#user-symptom").val();
+    const doctorName = $('#doctor-name').val();
+    $('#location').val("");
     console.log(userSymptom);
 
     (async () => {
       let getDoctors = new Doctors();
-      const response = await getDoctors.findDoctors();
+      const response = await getDoctors.findDoctors(doctorName, userSymptom);
       showDoctors(response);
     })();
 
     function showDoctors(response) {
-      
+      $('#name').text(`Name: ${response.data[0].profile.first_name} ${response.data[0].profile.last_name}`);
     }
-
-
   });
 });

69b814e18f72025a70fb1f07c34e55248af13cd1
wymerkd <wymerkd@gmail.com>
2019-11-24 18:31:58 -0800

    add url for to call API

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,7 @@
 export class Doctors {
   async getDoctors() {
     try {
-      let response = await fetch(``);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=f42c94ad008bb7ce74679e9d77369825`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {

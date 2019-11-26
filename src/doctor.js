3e72b7c905c89de65d457d102bba08eddf6ced9b
wymerkd <wymerkd@gmail.com>
2019-11-25 19:32:33 -0800

    add page breaks and <hr> to separate doctor results.

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,7 @@
 export class Doctors {
   async findDoctors(userSymptom) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&location=or-portland&skip=0&user_key=${process.env.API_KEY}`)
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&location=or-portland&skip=0&user_key=${process.env.API_KEY}`);
 
       let jsonifiedResponse = await response.json();
       console.log(jsonifiedResponse);
--- src/main.js
+++ src/main.js
@@ -17,11 +17,11 @@ $(document).ready(function() {
 
     function showDoctors(response) {
       for (let i = 0; i < response.data.length; i++) {
-        $('.results').append(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}
-           Phone: ${response.data[i].practices[0].phones[0].number}
-           Website: ${response.data[i].practices[0].website}
-           Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}
-           Accepting new patients: ${response.data[i].practices[0].accepts_new_patients} <br>`);
+        $('.results').append(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br>
+           Phone: ${response.data[i].practices[0].phones[0].number}<br>
+           Website: ${response.data[i].practices[0].website}<br>
+           Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}<br>
+           Accepting new patients: ${response.data[i].practices[0].accepts_new_patients}<hr> <br>`);
       }
     }
   });

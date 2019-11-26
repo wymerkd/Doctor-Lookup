0bf925f0b70e9a3b89919561530e7479efff4d14
wymerkd <wymerkd@gmail.com>
2019-11-25 23:22:05 -0800

    add if else statement to account for no website and if doctor is accepting new patients or not.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
+
 import { Doctors } from './../src/doctor.js';
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
@@ -16,16 +17,28 @@ $(document).ready(function() {
     })();
 
     function showDoctors(response) {
-      for (let i = 0; i < response.data.length; i++) {
-        $('.results').append(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br>
-           Phone: ${response.data[i].practices[0].phones[0].number}<br>
-           Website: ${response.data[i].practices[0].website}<br>
-           Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}<br>
-           Accepting new patients: ${response.data[i].practices[0].accepts_new_patients}<hr> <br>`);
+      if (response.data.length === 0) {
+        $('.results').text("No doctors meet the criteria");
+      } else {
+        for (let i = 0; i < response.data.length; i++) {
+          $('.results').append(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br>
+          Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}<br>
+          Phone: ${response.data[i].practices[0].phones[0].number}<br>`);
+          if (response.data[i].practices[0].website === undefined) {
+            $('.results').append(`Website: Not Available <br>`);
+          } else {
+            $('.results').append(`Website: ${response.data[i].practices[0].website}<br>`);
+          }
+          if (response.data[i].practices[0].accepts_new_patients === true) {
+            $('.results').append(`Currently accepting new patients <br> <hr>`);
+          } else {
+            $('.results').append(`Not currently accepting new patients <br> <hr>`);
+          }
+        }
       }
     }
-  });
-  $('#clear').click(function() {
-    $('.results').empty();
+    $('#clear').click(function() {
+      $('.results').empty();
+    });
   });
 });

b05154c21008ac26f277f04322bbfbea92fae359
wymerkd <wymerkd@gmail.com>
2019-11-25 19:14:52 -0800

    remove doctor search input field and combine with query input field to search for both doctor and symtom in same search field.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 import { Doctors } from './../src/doctor.js';
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css';
 
 $(document).ready(function() {
   $('#submit').click(function() {
     const userSymptom = $("#user-symptom").val();
-    const doctorName = $('#doctor-name').val();
-    $('#location').val("");
     console.log(userSymptom);
 
     (async () => {
       let getDoctors = new Doctors();
-      const response = await getDoctors.findDoctors(doctorName, userSymptom);
+      const response = await getDoctors.findDoctors(userSymptom);
       showDoctors(response);
     })();
 
     function showDoctors(response) {
       for (let i = 0; i < response.data.length; i++) {
-        $('.results').append(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} Phone: ${response.data[i].practices[0].phones[0].number} Website: ${response.data[i].practices[0].website} Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip} Accepting new patients: ${response.data[i].practices[0].accepts_new_patients} <br>`);
+        $('.results').append(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}
+           Phone: ${response.data[i].practices[0].phones[0].number}
+           Website: ${response.data[i].practices[0].website}
+           Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}
+           Accepting new patients: ${response.data[i].practices[0].accepts_new_patients} <br>`);
       }
     }
   });
